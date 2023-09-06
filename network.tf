1d6b08e90f3f1340e975e8c133eb193ff162f99a
Reanne Lee <leeml@pcs-security.com>
2023-09-06 05:24:18 +0000

    added network moduleee

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/leeml-org-azure2/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name =  "${var.prefix}-workshop"
 }
