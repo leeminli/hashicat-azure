83bc5e073f6b49c127e87134fa5afd35739ab10c
Reanne Lee <leeml@pcs-security.com>
2023-09-06 03:56:56 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,16 @@
+
+
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+module "network" {
+source  = "app.terraform.io/leeml-org-azure2/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
